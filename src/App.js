626444afdb85789b619efa00c6c9275423150f62
Arash <arash@wetaxi.it>
2023-02-17 15:19:58 +0100

    [ADD] instructions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import Hello from './Hello';
 
 function App() {
+  // excercise 1: fix the issue with Hello visibility
+  // provide an explanation in the comments
+
+  // EXERCISE 1 - EXPLANATION: [your explanation here]
   let showButton = false;
 
   const clickHandler = () => {
--- src/Hello.js
+++ src/Hello.js
@@ -1,4 +1,14 @@
 const Hello = () => {
+  // excercise 2 : add a log message when the component mounts "Hello mounted!"
+  // provide an explanation in the comments
+
+  // EXERCISE 2 - EXPLANATION: [your explanation here]
+
+
+  // excercise 2 : add a log message when the component unmounts "Hello unmounted!"
+  // provide an explanation in the comments
+
+  // EXERCISE 3 - EXPLANATION: [your explanation here]
   return <div className='text-container'>
     Hello! 😃
   </div>
